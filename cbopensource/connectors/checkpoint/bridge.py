a36b2bc927731699bb0f77e3f96324b7a5f64fc6
Jason McFarland <jmcfarland@carbonblack.com>
2018-04-16 21:31:21 -0500

    basics of md5 query working

--- cbopensource/connectors/checkpoint/bridge.py
+++ cbopensource/connectors/checkpoint/bridge.py
@@ -7,6 +7,10 @@ import logging
 import os
 import sys
 from checkpoint_api import checkpoint_query
+from cbapi.connection import CbAPISessionAdapter
+from checkpoint_api import APISession
+import json
+import traceback
 
 log = logging.getLogger(__name__)
 
@@ -19,12 +23,17 @@ class CheckpointProvider(BinaryAnalysisProvider):
         self.checkpoint_ssl_verify = checkpoint_ssl_verify
         self.current_api_key_index = 0
         self.work_directory = work_directory
+        self.session = APISession(api_key=self.api_key, throttle_per_minute=120)
+        tls_adapter = CbAPISessionAdapter(force_tls_1_2=True)
+        self.session.mount("https://", tls_adapter)
 
-    def _call_checkpoint_api(self, method, path, payload=None, files=None):
+    def _call_checkpoint_api(self, method, path, headers, payload=None, files=None):
         url = self.checkpoint_url + path
+        self.session.headers.update(headers)
 
         if method == 'GET':
             try:
+
                 r = self.session.get(url, verify=self.checkpoint_ssl_verify)
             except Exception as e:
                 log.exception("Exception when sending checkpoint API GET request: %s" % e)
@@ -44,12 +53,30 @@ class CheckpointProvider(BinaryAnalysisProvider):
         """
         query the checkpoint api to get a report on an md5
         """
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.api_key,
+        }
+
+        payload = json.dumps({
+            "request": [
+                {
+                    "md5": md5sum,
+                    "features": [
+                        "te",
+                        "av",
+                        "extraction"
+                    ]
+                }
+            ]
+        })
+
         log.info("Querying checkpoint for md5sum %s" % md5sum)
 
-        response = checkpoint_query(self.checkpoint_url, self.api_key, md5sum)
-
-        status_code, content = self._call_checkpoint_api("POST", "/publicapi/get/verdict",
-                                                         {'hash': md5sum.lower()})
+        status_code, content = self._call_checkpoint_api(method="POST",
+                                                         path="/tecloud/api/v1/file/query",
+                                                         headers=headers,
+                                                         payload=payload)
 
         if status_code == 404:
             return None  # can't find the binary
@@ -59,29 +86,63 @@ class CheckpointProvider(BinaryAnalysisProvider):
             raise AnalysisTemporaryError("Received unknown HTTP status code %d from checkpoint" % status_code,
                                          retry_in=120)
 
-        response = etree.fromstring(content)
+        dict_response = json.loads(content)
+        try:
+            checkpoint_status_code = dict_response.get("response", [])[0].get("status", {}).get("code", -1)
+        except Exception as e:
+            checkpoint_status_code = -1
+            log.error("Failed to parse checkpoint response JSON")
+            log.error(traceback.format_exc())
 
-        # Return 0 Benign verdict
-        # 1 Malware verdict
-        # 2 Grayware verdict
-        # -100 Verdict is pending
-        # -101 Indicates a file error
-        # -102 The file could not be found
-        # -103 The hash submitted is invalid
-        if md5sum.lower() == response.findtext("./get-verdict-info/md5").lower():
-            verdict = response.findtext("./get-verdict-info/verdict").strip()
-            if verdict == "-100":
-                return None  # waiting for checkpoint verdict
-            elif verdict == "-102":
-                return None  # file not in checkpoint yet
-            elif verdict.startswith("-"):
-                raise AnalysisPermanentError("checkpoint could not process file: error %s" % verdict)
-            elif verdict == "1":
-                return self.generate_malware_result(md5sum, 100)
-            elif verdict == "2":
-                return self.generate_malware_result(md5sum, 50)
-            else:
-                return AnalysisResult(score=0)
+        log.info(checkpoint_status_code)
+        time.sleep(10)
+
+        if checkpoint_status_code == 1001:
+            #
+            # Found
+            #
+            log.info(content)
+            return None
+            # return AnalysisResult(score=0)
+
+        elif checkpoint_status_code == 1003:
+            #
+            # Pending
+            #
+            return None
+        elif checkpoint_status_code == 1004:
+            #
+            # 1004 NOT_Found
+            # File is not in checkpoint yet
+            #
+            return None
+        elif checkpoint_status_code == 1005:
+            #
+            # Out of Quota
+            #
+            return AnalysisTemporaryError("Out of Quota")
+        elif checkpoint_status_code == 1006:
+            #
+            # Partially found
+            #
+            return None
+        elif checkpoint_status_code == 1007:
+            #
+            # FILE_TYPE_NOT_SUPPORTED
+            #
+            return AnalysisPermanentError("Filetype is not supported")
+        elif checkpoint_status_code == 1009:
+            #
+            # Internal Error
+            #
+            return AnalysisTemporaryError("Internal Error from Checkpoint")
+        elif checkpoint_status_code == 1011:
+            #
+            # Insufficient resources
+            #
+            return AnalysisTemporaryError("Checkpoint reports insufficient resources")
+        else:
+            return None
 
     def generate_malware_result(self, md5, score):
         status_code, content = self._call_checkpoint_api("POST", "/publicapi/get/report",
@@ -196,7 +257,9 @@ if __name__ == '__main__':
     my_path = os.path.dirname(os.path.abspath(__file__))
     temp_directory = "/tmp/checkpoint"
 
-    config_path = os.path.join(my_path, "testing.conf")
+    #/private/tmp/checkpoint/sqlite.db
+
+    config_path = "testing.conf"
     daemon = CheckpointConnector('checkpointtest',
                                  configfile=config_path,
                                  work_directory=temp_directory,
--- cbopensource/connectors/checkpoint/checkpoint_api.py
+++ cbopensource/connectors/checkpoint/checkpoint_api.py
@@ -5,6 +5,112 @@ import traceback
 
 logger = logging.getLogger(__name__)
 
+from threading import Lock
+from datetime import datetime, timedelta
+import time
+import logging
+from requests.adapters import HTTPAdapter
+from requests import Session
+import sys
+
+log = logging.getLogger(__name__)
+
+
+class APIQuotaExceededError(Exception):
+    pass
+
+
+class APISession(Session):
+    def __init__(self, request_quota=None, throttle_per_minute=None, api_key=None):
+        super(APISession, self).__init__()
+        self.request_quota = request_quota
+        self.throttle_per_minute = throttle_per_minute
+
+        self.api_key = api_key
+        self.current_api_key_index = 0
+        self.current_api_key_index_lock = Lock()
+
+        self.request_lock = Lock()
+
+        self.total_request_count = 0
+        self.total_reply_count = 0
+
+        # throttling
+        self.current_minute = datetime.now()
+        self.requests_current_minute = 0
+
+        # quota
+        self.current_quota_usage = 0
+
+        self.wait_until = None
+
+    # def apply_api_key(self, request):
+    #     if len(self.api_keys) == 0:
+    #         log.fatal("No valid API keys remaining, exiting")
+    #         sys.exit(2)
+    #
+    #     if request.method == "GET":
+    #         return request
+    #     elif type(request.data) == dict:
+    #         request.data['apikey'] = self.api_keys[self.current_api_key_index]
+    #
+    #     return request
+
+    def prepare_request(self, request):
+        #request = self.apply_api_key(request)
+        return super(APISession, self).prepare_request(request)
+
+    def send(self, request, **kwargs):
+        if self.request_quota:
+            self.current_quota_usage += 1
+            if self.current_quota_usage > self.request_quota:
+                self.set_wait_time()
+
+        if self.wait_until:
+            sleep_time = (self.wait_until - datetime.now()).total_seconds()
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+            self.wait_until = None
+
+        # next, apply throttling
+        if self.throttle_per_minute:
+            now = datetime.now()
+            if now - self.current_minute > timedelta(minutes=1):
+                self.current_minute = now
+                self.requests_current_minute = 0
+            else:
+                self.requests_current_minute += 1
+
+            if self.requests_current_minute > self.throttle_per_minute:
+                log.info("Too many API requests in the past 60 seconds (limit is {0}). Waiting one minute..."
+                         .format(self.throttle_per_minute))
+                time.sleep(60)
+                self.requests_current_minute = 0
+                self.current_minute = datetime.now()
+
+        return super(APISession, self).send(request, **kwargs)
+
+    def set_wait_time(self):
+        if self.wait_until:
+            return
+
+        # default quota strategy: wait an hour before trying again
+        next_time = datetime.now()
+        self.wait_until = next_time.replace(hour=(next_time.hour + 1))
+
+        log.info("Reached API quota limit for all API keys. Waiting until {0} to try again.".format(self.wait_until))
+        self.current_quota_usage = 0
+
+    def request(self, *args, **kwargs):
+        with self.request_lock:
+            successful = False
+
+            while not successful:
+                self.total_request_count += 1
+                response = super(APISession, self).request(*args, **kwargs)
+                self.total_reply_count += 1
+                return response
+
 
 def checkpoint_submit_binary():
     pass
